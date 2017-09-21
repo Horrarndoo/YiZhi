80fcfb8619cd1ea866004f18a0d95488881f869d
Horrarndoo <237077219@qq.com>
2017-09-21 18:08:13 +0800

    1.微信精选固定住Appbar高度，不显示Title，image，copyright等；

--- app/src/main/java/com/zyw/horrarndoo/yizhi/ui/activity/detail/WeixinChoiceDetailActivity.java
+++ app/src/main/java/com/zyw/horrarndoo/yizhi/ui/activity/detail/WeixinChoiceDetailActivity.java
@@ -2,11 +2,13 @@ package com.zyw.horrarndoo.yizhi.ui.activity.detail;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
 import android.view.View;
 
-import com.bumptech.glide.Glide;
 import com.zyw.horrarndoo.sdk.base.BasePresenter;
+import com.zyw.horrarndoo.sdk.utils.DisplayUtils;
 import com.zyw.horrarndoo.sdk.utils.ResourcesUtils;
+import com.zyw.horrarndoo.sdk.utils.StatusBarUtils;
 import com.zyw.horrarndoo.yizhi.R;
 import com.zyw.horrarndoo.yizhi.constant.BundleKeyConstant;
 import com.zyw.horrarndoo.yizhi.contract.detail.WeixinDetailContract;
@@ -38,10 +40,15 @@ public class WeixinChoiceDetailActivity extends BaseDetailActivity<WeixinDetailC
     @Override
     protected void initView(Bundle savedInstanceState) {
         super.initView(savedInstanceState);
-        tvDetailTitle.setText(mTitle);
-        tvDetailcopyright.setText(mCopyright);
-        Glide.with(mContext).load(mImageUrl).crossFade().into(ivDetail);
-        appBar.setExpanded(false);
+        //微信精选内部已经有Title等信息，直接显示webview内容
+        //tvDetailTitle.setText(mTitle);
+        //tvDetailcopyright.setText(mCopyright);
+        //Glide.with(mContext).load(mImageUrl).crossFade().into(ivDetail);
+        AppBarLayout.LayoutParams params = (AppBarLayout.LayoutParams) appBar.getChildAt(0)
+                .getLayoutParams();
+        // 控件的高强制设成56dp+状态栏高度
+        params.height = DisplayUtils.dp2px(56) + StatusBarUtils.getStatusBarHeight
+                (mContext);
     }
 
     @Override
