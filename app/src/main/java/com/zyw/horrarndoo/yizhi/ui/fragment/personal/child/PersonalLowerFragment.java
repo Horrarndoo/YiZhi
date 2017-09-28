6de39fe96b7f86dc84df5b8b67c6dc80a9a84c3d
Horrarndoo <237077219@qq.com>
2017-09-28 13:44:03 +0800

    1.个人界面增加关于选项；

--- app/src/main/java/com/zyw/horrarndoo/yizhi/ui/fragment/personal/child/PersonalLowerFragment.java
+++ app/src/main/java/com/zyw/horrarndoo/yizhi/ui/fragment/personal/child/PersonalLowerFragment.java
@@ -1,5 +1,6 @@
 package com.zyw.horrarndoo.yizhi.ui.fragment.personal.child;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.TextView;
 
 import com.zyw.horrarndoo.sdk.base.BaseCompatFragment;
 import com.zyw.horrarndoo.yizhi.R;
+import com.zyw.horrarndoo.yizhi.ui.activity.AboutActivity;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -20,6 +22,8 @@ public class PersonalLowerFragment extends BaseCompatFragment {
 
     @BindView(R.id.tv_btn_settings)
     TextView tvBtnSetting;
+    @BindView(R.id.tv_btn_about)
+    TextView tvBtnAbout;
 
     public static PersonalLowerFragment newInstance() {
         Bundle args = new Bundle();
@@ -38,11 +42,14 @@ public class PersonalLowerFragment extends BaseCompatFragment {
 
     }
 
-    @OnClick(R.id.tv_btn_settings)
+    @OnClick({R.id.tv_btn_settings, R.id.tv_btn_about})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.tv_btn_settings:
                 start(PersonalSettingFragment.newInstance());
+                break;
+            case R.id.tv_btn_about:
+                startActivity(new Intent(mActivity, AboutActivity.class));
                 break;
             default:
                 break;
