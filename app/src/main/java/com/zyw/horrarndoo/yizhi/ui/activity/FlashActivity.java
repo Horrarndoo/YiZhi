8c7aa2f11e6bdb8ef0899fc340e3431f9353ca1d
Horrarndoo <237077219@qq.com>
2017-10-08 16:42:41 +0800

    1.优化Flash界面加载，避免偶尔白屏的情况；

--- app/src/main/java/com/zyw/horrarndoo/yizhi/ui/activity/FlashActivity.java
+++ app/src/main/java/com/zyw/horrarndoo/yizhi/ui/activity/FlashActivity.java
@@ -1,27 +1,26 @@
 package com.zyw.horrarndoo.yizhi.ui.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.zyw.horrarndoo.sdk.base.BaseCompatActivity;
-import com.zyw.horrarndoo.yizhi.R;
 
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 import io.reactivex.functions.Consumer;
 
-public class FlashActivity extends BaseCompatActivity {
-
+public class FlashActivity extends Activity {
     private boolean mIsCancle;
 
     @Override
-    protected void initView(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         Observable.timer(500, TimeUnit.MILLISECONDS)
                 .subscribe(new Consumer<Long>() {
                     @Override
                     public void accept(Long l) throws Exception {
                         if (!mIsCancle) {
-                            startActivity(MainActivity.class);
+                            startActivity(new Intent(FlashActivity.this, MainActivity.class));
                             finish();
                         }
                     }
@@ -29,12 +28,8 @@ public class FlashActivity extends BaseCompatActivity {
     }
 
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_flash;
-    }
-
-    @Override
-    public void onBackPressedSupport() {
+    public void onBackPressed() {
+        super.onBackPressed();
         mIsCancle = true;
         finish();
     }
